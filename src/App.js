54a5696d74ec4e104001b0470790822b590d9c3a
Yaren Uludoğan <112831263+yarenuludogan@users.noreply.github.com>
2024-08-05 05:42:08 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import poke from './assets/poke.png';
 import { useNavigate } from 'react-router-dom';
 
+
 function App() {
   const [cards, setCards] = useState([]);
   const [search, setSearch] = useState('');
@@ -86,33 +87,34 @@ function App() {
   };
 
   return (
-    <div className="relative min-h-screen mb-20">
-      <nav className="bg-gray-800 p-4 text-white shadow-2xl flex items-center fixed top-0 left-0 right-0 z-50">
+    <div className="relative min-h-screen mb-20 ml-20 mr-20 ">
+      <nav className="bg-gray-800 p-6 text-white h-24 shadow-2xl shadow-slate-500 flex items-center fixed top-0 left-0 right-0 z-50">
         <div className="flex items-center">
           <img
             src={poke}
             alt="Pokémon Icon"
-            className="h-8 w-8 mr-2"
+            className="h-16 w-16 mr-4 mb-4 ml-4"
           />
-          <h1 className="text-xl font-bold">Pokédex</h1>
-        </div>
-      </nav>
-      <div className="">
-        <div className="container mx-auto p-4 mt-20 mb-12">
-          <div className="">
+          <h1 className="text-3xl font-bold mb-6">Pokédex</h1>
+        </div><div className="flex-grow flex justify-center ml-16 mr-12">
           <input
             type="text"
             placeholder="Search for a card"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
-            className="mb-8 mt-8 p-2 border-4 rounded-full w-full bg-gray-100 shadow-inner"
+            className="mb-8 mt-4 p-2 border-2 rounded-full w-full bg-gray-900 shadow-inner"
             style={{ textIndent:'20px'}} 
-          /></div>
+          />
+          </div>
+      </nav>
+      
+        <div className="container mx-auto p-4 mt-28 mb-12">
+          
           <div className="flex justify-end mb-4">
             <select 
               onChange={handleSortChange} 
               value={sortBy} 
-              className="p-2 border-2 rounded-3xl bg-gray-200 transition-transform duration-300 ease-in-out transform hover:scale-105 inner-shadow-3xl"
+              className="p-2 border-2 rounded-3xl bg-gray-200 transition-transform duration-300 ease-in-out transform hover:scale-105 inner-shadow-3xl shadow-2xl shadow-gray-300"
             >
               <option className="text-gray-600" value="nameatoz">Sort by Name (A-Z)</option>
               <option className="text-gray-600" value="nameztoa">Sort by Name (Z-A)</option>
@@ -123,34 +125,34 @@ function App() {
 
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
             {loading ? (
-              <div className="flex flex-col justify-center items-center w-40 min-h-24 mt-12 ml-96 ">
-                <div className="flex flex-col items-center justify-center">
-                  <div className="flex space-x-2 mb-4">
-                    <div className="h-10 w-10 bg-gray-700 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
-                    <div className="h-10 w-10 bg-gray-600 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
-                    <div className="h-10 w-10 bg-gray-500 rounded-full animate-bounce"></div>
-                  </div>
-                </div>
+              
+              <div className="col-span-full flex justify-center items-center mt-8">
+              <div className="flex space-x-4">
+                <div className="h-10 w-10 bg-gray-700 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+                <div className="h-10 w-10 bg-gray-600 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+                <div className="h-10 w-10 bg-gray-500 rounded-full animate-bounce"></div>
               </div>
+            </div>
+            
             ) : currentCards.length > 0 ? (
               currentCards.map(card => (
                 <div key={card.id}
-                 className="truncate ... border border-gray-200 p-4 rounded-3xl shadow-2xl flex flex-col transition-transform duration-300 ease-in-out transform hover:scale-105"
+                 className="truncate ... border border-gray-200 p-4 rounded-3xl shadow-2xl shadow-gray-400 flex flex-col transition-transform duration-300 ease-in-out transform hover:scale-105"
                  onClick={() => handleClick(card.id)}>
                     <h2 className="text-2xl font-sans font-bold pb-2 text-sky-900">{card.name}</h2>
                   <img
                     
                     src={`https://img.pokemondb.net/artwork/${card.name.toLowerCase()}.jpg`}
                     alt={card.name} 
-                    className="w-full items-center object-contain h-48 w-48  rounded-3xl transition-transform duration-300 ease-in-out transform hover:scale-105"
+                    className=" items-center object-contain h-48 w-48  rounded-3xl transition-transform duration-300 ease-in-out transform hover:scale-105"
                     onError={(e) => e.target.src = 'https://via.placeholder.com/150'}
                   />
                   
                   <div className="object-scale-down-30">
                     <p className="text-sky-900 text-sm font-sans truncate ..."><strong>HP</strong> {card.hp}</p>
-                    <p className="text-sky-900 text-sm font-sans truncate ..."><strong>Attacks</strong> {card.attacks?.map(attack => attack.name).join(', ')}</p>
+                   
                     <p className="text-sky-900 text-sm font-sans truncate ..."><strong>Weaknesses</strong> {card.weaknesses?.map(weakness => weakness.type).join(', ')}</p>
-                    <p className="text-sky-900 text-sm font-sans truncate ..."><strong>Set</strong> {card.set.name}</p>
+                    
                   </div>
                   
                   <div className="absolute bottom-2 right-2 text-gray-500 rounded-2xl w-12 h-8 flex items-center justify-center sm:w-14 sm:h-6 md:w-16 md:h-8 lg:w-18 lg:h-8 xl:w-20 xl:h-8">
@@ -169,8 +171,8 @@ function App() {
             )}
           </div>
         </div>
-      </div>
-      <footer className="bg-gray-800 text-stone-100 shadow-2xl fixed bottom-0 left-0 right-0 p-4 z-50">
+      
+      <footer className="bg-gray-800 text-stone-100 shadow-2xl shadow-slate-900 fixed bottom-0 left-0 right-0 p-4 z-50">
         <div className="flex justify-center">
           <div className="flex space-x-2">
             {Array.from({ length: totalPages }, (_, index) => (
